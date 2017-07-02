9f1c0e2e18e02bc1c0b3c59468364e75a8d95348
arvinh <arvin0731@gmail.com>
2017-07-02 17:45:30 +0800

    Day 8 Canvas

---
+++ Day-08-Fun with HTML5 Canvas/README.md
@@ -0,0 +1,24 @@
+## Key points
+
+1. There are four basic steps to start drawing on canvas:
+    * `ctx.beginPath();` 
+    * `ctx.moveTo(lastX, lastY);` // start from
+    * `ctx.lineTo(e.offsetX, e.offsetY);` // go to
+    * `ctx.stroke();`
+
+2. `globalCompositeOperation` can apply many different blend mode
+    * [CompositeOperation MDN](https://developer.mozilla.org/en-US/docs/Web/API/CanvasRenderingContext2D/globalCompositeOperation)
+
+3. Have to keep update `lastX` and `lastY` which is your start point. (Initialize at mousedown, then update it on drawing)
+
+4. Use `ctx.clearRect(0, 0, canvas.width, canvas.height)` to clean canvas.
+
+5. [lineCap](https://www.w3schools.com/tags/canvas_linecap.asp): style in each end of line.
+
+6. [lineJoin](https://www.w3schools.com/tags/canvas_linejoin.asp): style when two line meet.
+
+
+## Note
+
+* The first value in hsl is interesting, when value larger than 360, the color will repeat from zero while value keep growing. We reset it when larger than 360 though.
+* ES6 assign: [lastX, lastY] = [e.offsetX, e.offsetY];
---
+++ Day-08-Fun with HTML5 Canvas/canvas.js
@@ -0,0 +1,58 @@
+(function() {
+  const canvasWrapper = document.querySelector('.canvas-wrapper');
+  const canvas = document.querySelector('#draw');
+  const resetBtn = document.querySelector('.reset-btn');
+  const ctx = canvas.getContext('2d');
+  canvas.width = canvasWrapper.clientWidth;
+  canvas.height = canvasWrapper.clientHeight - 25; // 25 is for title height
+  ctx.strokeStyle = '#BADA55';
+  ctx.lineJoin = 'round';
+  ctx.lineCap = 'round';
+  ctx.lineWidth = 100;
+  // ctx.globalCompositeOperation = 'multiply';
+
+  let isDrawing = false;
+  let lastX = 0;
+  let lastY = 0;
+  let hue = 0;
+  let direction = true;
+
+  function draw(e) {
+    if (!isDrawing) return; // stop the fn from running when they are not moused down
+    ctx.strokeStyle = `hsl(${hue}, 100%, 50%)`;
+    ctx.beginPath();
+    // start from
+    ctx.moveTo(lastX, lastY);
+    // go to
+    ctx.lineTo(e.offsetX, e.offsetY);
+    ctx.stroke();
+    [lastX, lastY] = [e.offsetX, e.offsetY];
+
+    hue++;
+    if (hue >= 360) {
+        hue = 0;
+    }
+
+    if (ctx.lineWidth >= 100 || ctx.lineWidth <= 1) {
+        direction = !direction;
+    }
+
+    if (direction) {
+        ctx.lineWidth++;
+    } else {
+        ctx.lineWidth--;
+    }
+  }
+
+  canvas.addEventListener('mousedown', (e) => {
+    isDrawing = true;
+    [lastX, lastY] = [e.offsetX, e.offsetY];
+  });
+
+  canvas.addEventListener('mousemove', draw);
+  canvas.addEventListener('mouseup', () => isDrawing = false);
+  canvas.addEventListener('mouseout', () => isDrawing = false);
+
+  resetBtn.addEventListener('click', () => ctx.clearRect(0, 0, canvas.width, canvas.height));
+
+})();
